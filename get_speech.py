877bf82d4f6e17a99985bd77428b4521628e66cf
carolinesmith123 <smith.caroline87@gmail.com>
2016-02-12 14:45:38 +0000

    Update get_speech.py

--- get_speech.py
+++ get_speech.py
@@ -1,72 +1,54 @@
-import urllib
+from urllib2 import Request, urlopen, URLError
 import string
-import re
 from BeautifulSoup import *
 
 # Opens url from WSJ, parses it, finds all 'p' tags
-url = raw_input('Enter - ')
-if (len(url) < 1): url = 'http://blogs.wsj.com/washwire/2015/06/16/donald-trump-transcript-our-country-needs-a-truly-great-leader/'
-print url
-html = urllib.urlopen(url).read()
-soup = BeautifulSoup(html)
-tags = soup.findAll('p')
+
+input = raw_input('Enter URL or press enter - ') 
+if (len(input) < 1): input = 'http://blogs.wsj.com/washwire/2015/06/16/donald-trump-\
+transcript-our-country-needs-a-truly-great-leader/'
+url = Request(input)
+
+try:
+    url = urlopen(url).read()
+except URLError, e:
+    if hasattr(e, 'reason'):
+        print 'We failed to reach a server.'
+        print 'Reason: ', e.reason
+    elif hasattr(e, 'code'):
+        print 'The server couldn\'t fulfill the request.'
+        print 'Error code: ', e.code
+else:
+    print 'URL is good!'
+    print input
+
+    soup = BeautifulSoup(url)
+    tags = soup.findAll('p')
 
 # Appends text content to list from p tags containing the speech
-lst = []
-for i in range(len(tags)):
-    paragraph = tags[i]
-    if i < 5 or i > (len(tags) - 18):
-        continue
-    lst.append(paragraph.text)
+
+    lst = []
+    for i in range(len(tags)):
+        paragraph = tags[i]
+        if i < 5 or i > (len(tags) - 18):
+            continue
+        lst.append(paragraph.text)
     
+# Cleans speech (removes all audience comments, and replaces html entities)
+# Makes speech a single string and writes it to text file
 
-# Cleans the speech (removes all audience comments, and replaces bad html characters)
-# Makes speech as single string from list items
-# Writes the speech to a plain text file
+    slst = [str(i) for i in lst]
+    slst = [i for i in slst if not "AUDIENCE" in i]
 
-slst = [str(i) for i in lst]
-slst = [i for i in slst if not "AUDIENCE" in i]
-
-mapping = { '&#8217;': "'", '&#8212;': '', '&#8220;': '', '&#8221;': '', '&#8230;': '', \
-'TRUMP:': '', '(LAUGHTER)': '', '(APPLAUSE)': '', '(ph)': '', }
-for k, v in mapping.iteritems():
-    slst = [string.replace(i, k, v) for i in slst]
+    mapping = { '&#8217;': "'", '&#8212;': '', '&#8220;': '', '&#8221;': '', \
+    '&#8230;': '', 'TRUMP:': '', '(LAUGHTER)': '', '(APPLAUSE)': '', '(ph)': '', }
+    for k, v in mapping.iteritems():
+        slst = [string.replace(i, k, v) for i in slst]
         
 
-speech = ' '.join(slst)
-speech = speech.strip()
-print speech
+    speech = ' '.join(slst)
+    speech = speech.strip()
+    print speech
 
-with open('Trump_speech.txt', 'w') as text_file:
-    text_file.write(speech)
-
-    
-    
-# Opens the .txt file, reads it and removes punctuation
-# Makes a word count dictionary
-# Sorts by count in reverse, prints top 50 words over 4 letters long
-
-import re
-
-with open('Trump_speech.txt', 'r') as fh:
-    speech = fh.read()
-
-    speech_count = speech.lower()
-    speech_count = re.sub(r'[?|,|.|\'|!]',r'', speech_count)
-    speech_count = speech_count.split()
-    print 'Speech total word count:  ', len(speech_count)
-    
-    sdict = dict()
-    
-    for word in speech_count:
-        sdict[word] = sdict.get(word, 0) + 1
-    
-    
-    most_common = sorted([(value,key) for (key,value) in sdict.items() if len(key)>4], reverse=True)
-    print 'Trump_speech most frequent words dictionary:', '\n', most_common[0:50]
-    
-    print 'How many times says great!...', sdict['great']
-    print 'How many times says his own name...', sdict['trump']
-    print 'How many times says china...', sdict['china']
-    print 'How many times says mexico...', sdict['mexico']
-    print 'How many times says america...', sdict['america']
+    with open('Trump_speech.txt', 'w') as text_file:
+        text_file.write(speech)   
